7bf6c516965d3ebbe5a3dc6f2eda5f44e7257a0d
Scott K Logan <logans@cottsay.net>
2022-11-30 12:29:45 -0800

    Implement mechanism for reading python METADATA files

---
+++ colcon_python_project/metadata.py
@@ -0,0 +1,40 @@
+# Copyright 2022 Open Source Robotics Foundation, Inc.
+# Licensed under the Apache License, Version 2.0
+
+from email.parser import Parser
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from colcon_python_project.hook_caller_decorator \
+    import get_decorated_hook_caller
+
+
+async def load_metadata(desc):
+    """
+    Load metadata for a Python project using PEP 517.
+
+    :param desc: The package descriptor
+    """
+    hook_caller = get_decorated_hook_caller(desc)
+    with TemporaryDirectory() as md_dir:
+        md_name = await hook_caller.prepare_metadata_for_build_wheel(
+            metadata_directory=md_dir)
+        md_path = Path(md_dir) / md_name / 'METADATA'
+        with open(md_path) as f:
+            metadata = Parser().parse(f)
+    return metadata
+
+
+async def load_and_cache_metadata(desc):
+    """
+    Get the cached metadata for a package descriptor.
+
+    If the metadata has not been loaded yet, load and cache it.
+
+    :param desc: The package descriptor
+    """
+    metadata = desc.metadata.get('python_project_metadata')
+    if metadata is None:
+        metadata = await load_metadata(desc)
+        desc.metadata['python_project_metadata'] = metadata
+    return metadata
