colcon/colcon-python-project

commit d48d6832dcbf4b56d5e97c59d2b989e2aa4e9e25

Suppress an irrelevant informational message from setuptools

colcon_python_project/hook_caller/__init__.py
@@ -67,6 +67,11 @@ class AsyncHookCaller:
         """Get the name of the backend to call hooks on."""
         return self._backend_name
 
+    @property
+    def env(self):
+        """Get the environment variables to use when invoking hooks."""
+        return self._env
+
     async def list_hooks(self):
         """
         Call into the backend to list implemented hooks.
@@ -81,7 +86,7 @@ class AsyncHookCaller:
             self._backend_name]
         process = await run(
             args, None, self._stderr_callback,
-            cwd=self._project_path, env=self._env,
+            cwd=self._project_path, env=self.env,
             capture_output=True)
         process.check_returncode()
         hook_names = [
@@ -105,7 +110,7 @@ class AsyncHookCaller:
             have_callbacks = self._stdout_callback or self._stderr_callback
             process = await run(
                 args, self._stdout_callback, self._stderr_callback,
-                cwd=self._project_path, env=self._env, close_fds=False,
+                cwd=self._project_path, env=self.env, close_fds=False,
                 capture_output=not have_callbacks)
             process.check_returncode()
             with os.fdopen(os.dup(transport.parent_in), 'rb') as f:

colcon_python_project/hook_caller_decorator/setuptools.py
@@ -21,6 +21,14 @@ else:
     ESCAPE_HATCH = '--build-option'
 
 
+def _add_python_warnings(env, new_warnings):
+    warnings = env.get('PYTHONWARNINGS', '').split(',')
+    for new_warning in new_warnings:
+        if new_warning not in warnings:
+            warnings.append(new_warning)
+    env['PYTHONWARNINGS'] = ','.join(warnings)
+
+
 class SetuptoolsHookCallerDecoratorExtension(
     HookCallerDecoratorExtensionPoint
 ):
@@ -38,6 +46,10 @@ class SetuptoolsHookCallerDecoratorExtension(
             'setuptools.build_meta:__legacy__',
         ):
             return hook_caller
+        _add_python_warnings(hook_caller.env, (
+            'ignore:Editable installation:setuptools.warnings.InformationOnly:'
+            'setuptools.command.editable_wheel',
+            ))
         return SetuptoolsDecorator(hook_caller)
 
 

commit 5b2d7e43af0c87e14609b898a220b9551f5bbea5

Handle hook call without explicit environment vars
Fixes: d48d6832dcbf4

colcon_python_project/hook_caller/__init__.py
@@ -58,7 +58,7 @@ class AsyncHookCaller:
         """
         self._backend_name = backend_name
         self._project_path = str(project_path) if project_path else None
-        self._env = dict(env) if env else None
+        self._env = dict(env if env is not None else os.environ)
         self._stdout_callback = stdout_callback
         self._stderr_callback = stderr_callback
 